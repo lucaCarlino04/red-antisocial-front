c43fb26f0311063c082b53828f5d6455a9e8403b
David555acosta <148257651+David555acosta@users.noreply.github.com>
2026-06-29 23:23:18 -0300

    se cambio ruta protegida de /iniciarSesion a /iniciar

--- src/components/RutaProtegida.tsx
+++ src/components/RutaProtegida.tsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext";
 
 export default function RutaProtegida() {
   const { user } = useAuth();
-  if (!user) return <Navigate to="/iniciarSesion" replace />;
+  if (!user) return <Navigate to="/iniciar" replace />;
 
   return <Outlet />;
 }
