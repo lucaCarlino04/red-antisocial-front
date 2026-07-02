d64556dd19f0008c2943c06161a045d2b50a5dd7
lucaCarlino04 <lucacarlinounahur@gmail.com>
2026-07-02 01:44:09 -0300

    Preparada la variable de entorno para el deploy

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,4 @@
-export const URL = "http://localhost:3000/api";
+// Si existe la variable de Vercel, le sumamos el "/api" automáticamente. Si no, usa localhost
+export const URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : "http://localhost:3000/api";
